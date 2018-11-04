eec163dd48fcb21b1a9f4879ea5e9130fdf289a7
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-11-03 22:01:21 -0500

    Remove state and add .app code

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react'
 import BookShelf from './BookShelf'
 
 class Library extends Component {
-      state = {
-          /**
-           * TODO: Instead of using this state variable to keep track of which page
-           * we're on, use the URL in the browser's address bar. This will ensure that
-           * users can use the browser's back and forward buttons to navigate between
-           * pages, as well as provide a good URL they can bookmark and share.
-           */
-          showSearchPage: false
-      }
+      
 
     render() {
         return (
-            <div>
-                <BookShelf />
+            <div className="app">
+                <div className="list-books">
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className="list-books-content">
+                        <div>
+                            <BookShelf />
+                        </div>
+                    </div>
+                    <div className="open-search">
+                        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                    </div>
+                </div>
             </div>
         )
     }
 }
-
 export default Library
