94080fcffa0245e817f7d3cfb4f5c87cee65c1e9
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-11-10 20:01:27 -0600

    Create shelves and add props to Bookshelf

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react'
 import BookShelf from './BookShelf'
 
-class Library extends Component {
-      
+class Library extends Component { 
+    refreshShelves = () => {
+        const currentRead = {
+            title: "Currently Reading",
+            books: this.props.books.filter(book => book.shelf === 'currentlyReading')
+        };
+        const wantRead = {
+            title: "Want to Read",
+            books: this.props.books.filter(book => book.shelf === 'wantToRead')
+        };
+        const hasRead = {
+            title: "Read",
+            books: this.props.books.filter(book => book.shelf === 'read')
+        };
+
+        return ([currentRead, wantRead, hasRead])
+    }
 
     render() {
+        let shelves = [];
+        if (this.props.books && this.props.books.length) {
+            shelves = this.refreshShelves();
+        }
+
         return (
             <div className="app">
                 <div className="list-books">
                     <div className="list-books-title">
-                        <h1>MyReads</h1>
+                        <h1>My Reads</h1>
                     </div>
                     <div className="list-books-content">
                         <div>
-                            <BookShelf />
+                            {shelves && shelves.map((shelf) => (
+                                <BookShelf key={shelf.title} shelf={shelf} />
+                            ))}
                         </div>
                     </div>
                     <div className="open-search">
-                        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                        <a onClick={() => this.setprops({ showSearchPage: true })}>Add a book</a>
                     </div>
                 </div>
             </div>
