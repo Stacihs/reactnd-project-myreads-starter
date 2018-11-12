c39dd5692583c5cf4f43b4b8d5e6091a9cd3dc03
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-11-11 22:37:00 -0600

    Add functionality to BookShelfComponent

--- src/components/BookShelfControl.js
+++ src/components/BookShelfControl.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 
 class BookShelfControl extends Component {
+     state = {
+         shelfStatus: this.props.book.shelf || "none"
+     }
+
+     onSwitchShelf = (book, shelf) => {
+         this.setState({shelfStatus: shelf});
+         this.props.onSwitchShelf(book, shelf);
+     }
+
+     componentWillReceiveProps = (props) => {
+         this.props = props;
+         this.setState({shelfStatus: this.props.book.shelf});
+     }
+
     render() {
+
+
         return (
             <div className="book-shelf-changer">
-                <select>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
+                < select value={this.state.shelfStatus} onChange={(event) => this.onSwitchShelf(this.props.book, event.target.value)}>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
                 </select>
             </div>
         )
