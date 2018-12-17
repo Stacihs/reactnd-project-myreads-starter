9509654ce54bc5cd87e9d2653a2a15524a061aed
Staci Holmes-Segarra <stacihs@hotmail.com>
2018-12-17 02:04:08 -0600

    Match search state with library state

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from '../BooksAPI'
 import Book from './Book'
+import escapeRegExp from 'escape-string-regexp'
 
 class Search extends Component {
     state = {
         query: "",
-        searchResults: []
+        searchResults: [],
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then(response => {
+            this.setState({
+                books: response
+            })
+        })
     }
 
     updateQuery = (query) => {
@@ -19,16 +29,25 @@ class Search extends Component {
 
     updateSearchResults = (query) => {
         if (query) {
-            BooksAPI.search(query.trim()).then((searchResults) => {
+            BooksAPI.search(query).then(response => {
+                console.log(response);
                 // No books display upon error
-                if(searchResults.error) {
+                if(response.error) {
                     this.setState({
                         searchResults: []
                     });
                 } else {
                 //Show all books that match to user input in search bar
+                    response.forEach(b => {
+                        let searchResult = this.state.books.filter(B => B.id === b.id);
+                        b.shelf = searchResult[0] ? searchResult.shelf : undefined;
+                        if (searchResult[0]) {
+                            console.log('match');
+                            b.shelf = searchResult[0].shelf;
+                        }
+                    });
                     this.setState({
-                        searchResults: searchResults
+                        searchResults: response
                     })    
                 }
             })
@@ -41,6 +60,13 @@ class Search extends Component {
     }
     
     render() {
+        // let filteredResults
+        // if (this.state.query) {
+        //     const matchedResults = new RegExp(escapeRegExp(this.state.query), 'i')
+        //     filteredResults = this.props.books.filter((book) => matchedResults.test(book.title) || matchedResults.test(book.authors))
+        // } else {
+        //     filteredResults = this.props.books
+        // }
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -56,9 +82,9 @@ class Search extends Component {
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                        {this.state.searchResults.map(searchResult => (
-                            <li key={searchResult.id}>
-                                <Book book={searchResult} onSwitchShelf={this.props.onSwitchShelf}/>
+                        {this.state.searchResults.map(book => (
+                            <li key={book.id}>
+                                <Book book={book} onSwitchShelf={this.props.onSwitchShelf}/>
                             </li>
                         ))}
                     </ol>
